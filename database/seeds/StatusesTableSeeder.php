7c98c04bdb1a638d7c88ed389810a4b695748983
kspaper_0 <15380324@qq.com>
2018-12-12 06:16:22 +0000

    Add user statuses

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Status;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_ids = ['1','2','3'];
+        $faker = app(Faker\Generator::class);
+
+        //app() 方法来获取一个 Faker 容器 的实例，并借助
+
+        $statuses = factory(Status::class)->times(100)->make()->each(function ($status) use ($faker, $user_ids) {
+            $status->user_id = $faker->randomElement($user_ids);
+
+            //randomElement 方法来取出用户 id 数组中的任意一个元素并赋值给微博的 user_id
+        });
+
+        Status::insert($statuses->toArray());
+    }
+}
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="jumbotron">
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h3>Blog List</h3>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+  <div class="jumbotron">
     <h1>Home Page</h1>
     <p class="lead">
         lorem..
@@ -11,6 +27,7 @@
             Sign Up
         </a>
     </p>
-</div>
+  </div>
+  @endif
 
 @stop
